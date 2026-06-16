1136252ebc1aff658016f60a2b5e1a25501ca4e0
TheAceOfficials <acegamerytofficial@gmail.com>
2026-06-16 14:03:27 +0530

    Fix plugin bug, optimize package dependencies, and update final zip hash

--- jellyfin-btttr-plugin/obj/Release/net8.0/BtttrPosterPlugin.AssemblyInfo.cs
+++ jellyfin-btttr-plugin/obj/Release/net8.0/BtttrPosterPlugin.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Jellyfin.Plugin.BtttrPosters")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("3.1.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("3.1.0.0+d40b38dd5e40ff3144fdcee9ad7c23161558eb04")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("3.1.0.0+341d0c99d6db7e2ad4738bf28bfb06d417e185c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Jellyfin.Plugin.BtttrPosters")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Jellyfin.Plugin.BtttrPosters")]
 [assembly: System.Reflection.AssemblyVersionAttribute("3.1.0.0")]
